28ec07debf6ee737776cd5a48295384d89c07702
Stephen Lee <smlee.981@gmail.com>
2021-01-05 23:07:24 -0600

    add navbar response to scroll

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = () => scrollFunction();
+
+function scrollFunction() {
+  let menu = document.getElementById("menu");
+
+  // if you are scrolling
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    menu.style.borderBottom = "2px solid rgb(65, 65, 65)";
+    menu.style["boxShadow"] = "5px 10px 18px #888888";
+
+    // find and replace the navbar padding 
+    // if scrolling, smaller navbar
+    menu.className = menu.className.replace(/py-\d/, 'py-3');
+  }
+
+  // if you are not scrolling
+  else {
+    menu.style["boxShadow"] = "none";
+    menu.style.borderBottom = "none";
+
+    // find and replace the navbar padding 
+    // if not scrolling, bigger navbar
+    menu.className = menu.className.replace(/py-\d/, 'py-4');
+  }
+}
